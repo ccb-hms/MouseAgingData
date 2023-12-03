3f9298541dc3eb4e3733eed2701085b116400768
tram22nguyen <tram_nguyen@hms.harvard.edu>
2023-12-03 15:11:36 -0500

    added R, man, data folders

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Parabiosis Data
+#'
+#' Filtered metadata for 10X Genomics parabiosis experiment
+#'
+#' @format ## parabiosis_metadata
+#' A data frame with 105329 rows and 12 columns:
+#' \describe{
+#'   \item{barcode}{cell barcode}
+#'   \item{nCount_RNA}{number of RNA transcripts}
+#'   \item{animal name}{isochronic or parabionic conditions}
+#'   ...
+#' }
+#'
+#' @source <https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE222510>
+#' @examples
+#' data(parabiosis_metadata) # Lazy loading
+"parabiosis_metadata"
