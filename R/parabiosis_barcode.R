14c5ca4f490ecd7ce66c4800712712f8d93da549
tram22nguyen <tram_nguyen@hms.harvard.edu>
2023-12-07 18:39:47 -0500

    added parabiosis RDS and man

---
+++ R/parabiosis_barcode.R
@@ -0,0 +1,13 @@
+#' Barcodes for Parabiosis Filtered Dataset
+#'
+#' Unique cell barcodes for parabiosis filtered 10X Genomics dataset.
+#'
+#' @format ## A data frame with 1 column and 105329 rows:
+#' \describe{
+#'   \item{barcode}{unique cell barcode.}
+#' }
+#'
+#' @source <https://www.nature.com/articles/s43587-023-00373-6>
+#' @examples
+#' data(parabiosis_barcode) # Lazy loading
+"parabiosis_barcode"
