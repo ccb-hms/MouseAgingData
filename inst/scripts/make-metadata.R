c5e076b273bc2585f41e5079e263f8ddb35ca812
tram-nguyen-n <tram_nguyen@hms.harvard.edu>
2024-03-06 21:13:38 +0000

    revert changes

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -24,31 +24,10 @@ Parabiosis10x <- data.frame(
     stringsAsFactors = FALSE
 )
 
-# Brain10x_2019NN
-Brain10x_2019NN <- data.frame(
-    Title = "Brain10x_2019NN",
-    Description = paste0("10x Genomics processed single-cell dataset ",
-                         "from Ximerakis et al. 2019"),
-    BiocVersion = "3.19",
-    Genome = "mm10",
-    SourceType = "RDS",
-    SourceUrl = "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE129788",
-    SourceVersion = NA,
-    Species = "Mus musculus",
-    TaxonomyId = 10090,
-    Coordinate_1_based = NA,
-    DataProvider = "Lee Rubin at Harvard Stem Cell Institute",
-    Maintainer = "Tram Nguyen <Tram_Nguyen@hms.harvard.edu>",
-    RDataClass = "SingleCellExperiment",
-    DispatchClass = "RDS",
-    RDataPath = "MouseAgingData/brain10x_2019NN",
-    stringsAsFactors = FALSE
-)
 
 # Combined meta-data
 df_all <- base::rbind(
-    Parabiosis10x, 
-    Brain10x_2019NN
+    Parabiosis10x
 )
 
 # Save .csv file
